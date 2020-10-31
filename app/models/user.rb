09f06b01424663ca4fa43f075aabcea660132924
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-10-30 22:11:00 -0400

    Fixing rubocop errors of mutableconstant and emptylinesaroundclassbody

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 # rubocop : disable Layout/EndOfLine
 
+# rubocop : disable Style/MutableConstant
+
 class User < ApplicationRecord
   validates :username, presence: true
 
   validates :password, presence: true
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-
+  validates :email, presence: true, format: { with: valid_email_regex }
 end
+
+# rubocop : enable Style/MutableConstant
 
 # rubocop : enable Layout/EndOfLine
